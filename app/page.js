19c83926c6aaf3edf3d18796a181e046e3ba9a04
Souvik <souvik.basak2404@gmail.com>
2025-05-04 09:13:33 +0530

    changes in hero images

--- app/page.js
+++ app/page.js
@@ -1,17 +1,13 @@
 import FeaturedPosts from "@/components/FeaturedPosts"
-import Footer from "@/components/Footer"
-import Header from "@/components/Header"
 import HeroImages from "@/components/HeroImages"
 import Testimonials from "@/components/Testimonials"
 
 const page = () => {
   return (
     <>
-    <Header/>
     <HeroImages/>
     <Testimonials/>
     <FeaturedPosts/>
-    <Footer/>
     </>
   )
 }
