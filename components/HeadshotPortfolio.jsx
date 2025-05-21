a7ddedbf16f16ea3c79625883b6a552e9894055f
katotemanish28 <katotemanish@gmail.com>
2025-05-21 17:26:09 +0530

    headshotgenre title

--- components/HeadshotPortfolio.jsx
+++ components/HeadshotPortfolio.jsx
@@ -62,7 +62,7 @@ export default function FashionPortfolio() {
         <div className="mb-6">
           <Image
             style={{ marginTop: -72, marginLeft: -38 }}
-            src="/HeadshotsGenre.svg"
+            src="/HeadshotGenre.png"
             alt="Headshot Title"
             width={415}
             height={80}
